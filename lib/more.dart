94c0be06511796c2f24fc034c6065b4567a5c8c6
eddrinexhakasadiq18@gmail.com <kalembaeddy@gmail.com>
2023-01-02 19:57:37 +0300

    adding a tile in the more page

--- lib/more.dart
+++ lib/more.dart
@@ -1,13 +1,40 @@
 import 'package:flutter/material.dart';
-import 'package:sportsapp/Custom_app_Bar.dart';
 
 class more extends StatelessWidget {
   const more({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: CustomAppBar(titletext: "MORE"),
+    return Scaffold(
+      appBar: AppBar(title: const Text('More')),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          // ignore: prefer_const_literals_to_create_immutables
+          children: [
+            const ListTile(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(25),
+                      topRight: Radius.circular(25),
+                      bottomRight: Radius.circular(10),
+                      bottomLeft: Radius.circular(10))),
+              tileColor: Colors.indigo,
+              textColor: Colors.white,
+              iconColor: Colors.white,
+              leading: Icon(Icons.light),
+              title: Text('Signin/ Create New Account'),
+              //subtitle: Text('This is a subtitle'),
+            ),
+
+            const Divider(
+              height: 50,
+            ),
+            
+          ],
+        ),
+      ),
+
 
       
     );
